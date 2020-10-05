76af1d783093755a5853af798164e76191773527
sarathyweb <sarathywebindia@gmail.com>
2020-10-05 21:44:44 +0530

    Updated tailwind CSS breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,16 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [
-    './templates/*.html'
-  ],
+  // purge: [
+  //   './templates/**/*.html'
+  // ],
   theme: {
+    screens: {
+      'sm': {'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px'},
+    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
