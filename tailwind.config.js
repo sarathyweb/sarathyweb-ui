5d6f102785128290653700505da64f494a9b502b
Partha Sarathy T <sarathywebindia@gmail.com>
2020-10-31 00:38:56 +0530

    Removed files that are no longer needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,6 @@ module.exports = {
   //   './templates/**/*.html'
   // ],
   theme: {
-    screens: {
-      'sm': {'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px'},
-    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
